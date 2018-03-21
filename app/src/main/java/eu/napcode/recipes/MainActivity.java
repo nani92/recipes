beea83817f1dee1349e5cd3e078ac0b1ef179e57
Natalia Jastrzębska <nataliak@10g.pl>
2018-03-21 15:32:49 +0100

    setting up rv

--- app/src/main/java/eu/napcode/recipes/MainActivity.java
+++ app/src/main/java/eu/napcode/recipes/MainActivity.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 
 import javax.inject.Inject;
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MainViewModel mainViewModel;
+    private RecipesAdapter recipesAdapter;
 
     private boolean isTwoPane;
 
@@ -32,9 +34,16 @@ public class MainActivity extends AppCompatActivity {
 
         AndroidInjection.inject(this);
         isTwoPane = this.binding.listLayout.recipeDetailContainer != null;
+        setupRecyclerView();
 
         mainViewModel = ViewModelProviders
                 .of(this, viewModelFactory)
                 .get(MainViewModel.class);
+    }
+
+    private void setupRecyclerView() {
+        this.binding.listLayout.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        this.recipesAdapter = new RecipesAdapter();
+        this.binding.listLayout.recyclerView.setAdapter(recipesAdapter);
     }
 }
---
+++ app/src/main/java/eu/napcode/recipes/RecipesAdapter.java
@@ -0,0 +1,52 @@
+package eu.napcode.recipes;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.napcode.recipes.databinding.ItemRecipeBinding;
+import eu.napcode.recipes.model.Recipe;
+
+class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipeViewHolder>{
+
+    private List<Recipe> recipes = new ArrayList<>();
+
+    public void setRecipes(List<Recipe> recipes) {
+        this.recipes = recipes;
+
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemRecipeBinding itemRecipeBinding = DataBindingUtil.inflate(layoutInflater, R.layout.item_recipe, parent, false);
+
+        return new RecipeViewHolder(itemRecipeBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeViewHolder holder, int position) {
+        Recipe recipe = recipes.get(position);
+
+        holder.itemRecipeBinding.recipeNameTextView.setText(recipe.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+        public final ItemRecipeBinding itemRecipeBinding;
+
+        public RecipeViewHolder(ItemRecipeBinding itemRecipeBinding) {
+            super(itemRecipeBinding.getRoot());
+            this.itemRecipeBinding = itemRecipeBinding;
+        }
+    }
+}
