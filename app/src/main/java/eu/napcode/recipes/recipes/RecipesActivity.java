ccf62a5ddf31813897a81383a0f5dabb9f1620f5
Natalia Jastrzębska <nataliak@10g.pl>
2018-04-04 18:51:17 +0200

    displaying recipes

--- app/src/main/java/eu/napcode/recipes/recipes/RecipesActivity.java
+++ app/src/main/java/eu/napcode/recipes/recipes/RecipesActivity.java
@@ -2,10 +2,12 @@ package eu.napcode.recipes.recipes;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.databinding.DataBindingUtil;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 
 import java.util.List;
@@ -25,6 +27,8 @@ import static eu.napcode.recipes.recipedetails.RecipeDetailsActivity.RECIPE_NAME
 
 public class RecipesActivity extends AppCompatActivity implements RecipesAdapter.RecipeClickListener {
 
+    private static final int LANDSCAPE_COLUMN_COUNT = 2;
+    private static final int TABLET_LANDSCAPE_COLUMN_COUNT = 3;
     @Inject
     ViewModelFactory viewModelFactory;
 
@@ -75,9 +79,29 @@ public class RecipesActivity extends AppCompatActivity implements RecipesAdapter
     }
 
     private void setupRecyclerView() {
-        this.binding.recipesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        setupLayoutManager();
         this.recipesAdapter = new RecipesAdapter(this, this);
         this.binding.recipesRecyclerView.setAdapter(recipesAdapter);
+    }
+
+    private void setupLayoutManager() {
+        
+        if (getResources().getBoolean(R.bool.isTablet)) {
+
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                this.binding.recipesRecyclerView.setLayoutManager(new GridLayoutManager(this, LANDSCAPE_COLUMN_COUNT));
+            } else {
+                this.binding.recipesRecyclerView.setLayoutManager(new GridLayoutManager(this, TABLET_LANDSCAPE_COLUMN_COUNT));
+            }
+
+            return;
+        }
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            this.binding.recipesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        } else {
+            this.binding.recipesRecyclerView.setLayoutManager(new GridLayoutManager(this, LANDSCAPE_COLUMN_COUNT));
+        }
     }
 
     @Override
