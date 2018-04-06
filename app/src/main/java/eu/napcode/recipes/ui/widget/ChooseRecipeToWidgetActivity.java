24236f2ed7ea31031e4376f96598ac5484518299
Natalia Jastrzębska <nataliak@10g.pl>
2018-04-06 18:13:43 +0200

    display widget

--- app/src/main/java/eu/napcode/recipes/ui/widget/ChooseRecipeToWidgetActivity.java
+++ app/src/main/java/eu/napcode/recipes/ui/widget/ChooseRecipeToWidgetActivity.java
@@ -2,8 +2,10 @@ package eu.napcode.recipes.ui.widget;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +19,7 @@ import dagger.android.AndroidInjection;
 import eu.napcode.recipes.R;
 import eu.napcode.recipes.databinding.ActivityChooseRecipeToWidgetBinding;
 import eu.napcode.recipes.dependency.modules.viewmodel.ViewModelFactory;
+import eu.napcode.recipes.model.Ingredient;
 import eu.napcode.recipes.model.Recipe;
 import eu.napcode.recipes.repository.Resource;
 import eu.napcode.recipes.ui.recipes.RecipesViewModel;
@@ -31,6 +34,7 @@ public class ChooseRecipeToWidgetActivity extends AppCompatActivity {
 
     private ActivityChooseRecipeToWidgetBinding binding;
     private int appWidgetId;
+    private List<Recipe> recipes;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,7 +49,7 @@ public class ChooseRecipeToWidgetActivity extends AppCompatActivity {
                 .observe(this, this::processResponse);
         initializeAppWidget();
 
-        this.binding.addWidgetButton.setOnClickListener(view -> setActivityResult());
+        this.binding.addWidgetButton.setOnClickListener(view -> saveToSharedPrefs());
     }
 
     private void processResponse(Resource<List<Recipe>> listResource) {
@@ -63,15 +67,17 @@ public class ChooseRecipeToWidgetActivity extends AppCompatActivity {
         }
 
         if (listResource.status == Resource.Status.SUCCESS) {
+            this.recipes = listResource.data;
             displayRecipes(listResource.data);
         }
     }
 
     private void displayRecipes(List<Recipe> recipes) {
 
-        for (Recipe recipe: recipes) {
+        for (Recipe recipe : recipes) {
             RadioButton radioButton = new RadioButton(this);
             radioButton.setText(recipe.getName());
+            radioButton.setId(recipe.getId());
             this.binding.radioGroup.addView(radioButton);
         }
     }
@@ -80,7 +86,7 @@ public class ChooseRecipeToWidgetActivity extends AppCompatActivity {
         Snackbar.make(this.binding.container, message, Snackbar.LENGTH_LONG);
     }
 
-    private void initializeAppWidget(){
+    private void initializeAppWidget() {
         this.appWidgetId = INVALID_APPWIDGET_ID;
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -94,11 +100,65 @@ public class ChooseRecipeToWidgetActivity extends AppCompatActivity {
         }
     }
 
+    private void saveToSharedPrefs() {
+        Recipe recipe = getChosenRecipe();
+
+        if (recipe == null) {
+            setFailResult();
+
+            return;
+        }
+
+        StringBuilder saveRecipeString = new StringBuilder();
+        saveRecipeString.append(recipe.getName() + "\n");
+
+        for (Ingredient ingredient : recipe.getIngredients()) {
+            saveRecipeString.append("- ");
+            saveRecipeString.append(ingredient.getIngredient());
+            saveRecipeString.append("\n");
+        }
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences
+                .edit()
+                .putString("" + appWidgetId, saveRecipeString.toString())
+                .apply();
+
+
+        setActivityResult();
+    }
+
+    private Recipe getChosenRecipe() {
+
+        for (Recipe recipe : recipes) {
+
+            if (recipe.getId() == this.binding.radioGroup.getCheckedRadioButtonId()) {
+                return recipe;
+            }
+        }
+
+        return null;
+    }
+
     private void setActivityResult() {
         Intent resultValue = new Intent();
         resultValue.putExtra(EXTRA_APPWIDGET_ID, this.appWidgetId);
         setResult(RESULT_OK, resultValue);
 
+        updateWidget();
         finish();
+    }
+
+    private void setFailResult() {
+        setResult(RESULT_CANCELED);
+
+        finish();
+    }
+
+    private void updateWidget() {
+        Intent refreshIntent = new Intent(this, RecipeWidgetProvider.class);
+        refreshIntent.setAction(RecipeWidgetProvider.ACTION_RELOAD);
+        refreshIntent.putExtra(RecipeWidgetProvider.EXTRA_WIDGET_ID, appWidgetId);
+        sendBroadcast(refreshIntent);
     }
 }
--- app/src/main/java/eu/napcode/recipes/ui/widget/RecipeWidgetProvider.java
+++ app/src/main/java/eu/napcode/recipes/ui/widget/RecipeWidgetProvider.java
@@ -5,6 +5,11 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.preference.PreferenceManager;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
 import android.widget.RemoteViews;
 
 import eu.napcode.recipes.R;
@@ -12,7 +17,8 @@ import eu.napcode.recipes.ui.recipes.RecipesActivity;
 
 public class RecipeWidgetProvider extends AppWidgetProvider {
 
-    private CharSequence ingredientsSpannable;
+    public static final String ACTION_RELOAD = "reload";
+    public static final String EXTRA_WIDGET_ID = "widget id";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -23,17 +29,38 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
         for (int i = 0; i < widgetsCount; i++) {
             int appWidgetId = appWidgetIds[i];
 
-            Intent intent = new Intent(context, RecipesActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_recipe);
-            views.setTextViewText(R.id.widgetIngredientsTextView, getIngredientsSpannable());
-
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            updateView(context, appWidgetManager, appWidgetId);
         }
     }
 
-    public CharSequence getIngredientsSpannable() {
-        return ingredientsSpannable;
+    private void updateView(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        Intent intent = new Intent(context, RecipesActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_recipe);
+        views.setTextViewText(R.id.widgetIngredientsTextView, getIngredientsSpannable(context, appWidgetId));
+        views.setOnClickPendingIntent(R.id.widgetIngredientsTextView, pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    public SpannableString getIngredientsSpannable(Context context, int widgetId) {
+        String recipe = PreferenceManager.getDefaultSharedPreferences(context).getString("" + widgetId, "");
+
+        if (recipe.isEmpty()) {
+            return new SpannableString("");
+        }
+
+        SpannableString spannableString = new SpannableString(recipe);
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), 0, recipe.indexOf('\n'), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return spannableString;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        updateView(context, AppWidgetManager.getInstance(context), intent.getIntExtra(EXTRA_WIDGET_ID, 0));
     }
 }
